2d624817aa1e03f4185b0e3fdefc6b74a514d2ef
champ <itschetanshinde@gmail.com>
2020-08-10 20:23:02 +0530

    removed old navbar and sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Toolbar from './components/Toolbar/Toolbar';
-import Sidebar from './components/Sidebar/Sidebar';
+
+
 import Backdrop from './components/Backdrop/Backdrop'
 // import SigInform from './components/Form/Signin'
 import RegForm from './components/Form/Register'
@@ -8,31 +8,17 @@ import './App.css';
 import AllDishComponent from './components/AllDish/AllDishComponent';
 
 class App extends Component {
-  state = {
-    sidebarOpen: false
-  }
+ 
 
-  sidebarToggleClickHandler = () => {
-    this.setState((prevState) => {
-      return { sidebarOpen: !prevState.sidebarOpen }
-    });
-  };
+  
 
   render() {
-    let sidebar;
-    let backdrop;
-
-    if (this.state.sidebarOpen) {
-      sidebar = <Sidebar />
-      backdrop = <Backdrop />
-    }
+    
 
     return (
       <>
         <div className="App">
-          <Toolbar sidebarClickHandler={this.sidebarToggleClickHandler} />
-          {sidebar}
-          {backdrop}
+        
 
           <main style={{ marginTop: '80px' }}>
             <p> Some random text </p>
